53c6e7136ff8196af2e3cd090b91fbdf6c2f8fcc
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2024-10-10 23:17:34 +1300

    update views documentation

--- members_app/workouts/views.py
+++ members_app/workouts/views.py
@@ -7,6 +7,9 @@ from rest_framework.decorators import api_view, permission_classes
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def workouts_home(request):
+    """
+    Home page for workouts.
+    """
     workouts = Workout.objects.all()
     return render(request, "workouts/workouts_home.xml", {"workouts": workouts})
 
@@ -35,6 +38,11 @@ def list_assigned_workouts(request):
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def workout_detail(request, workout_id):
+    """
+    Display details for a specific workout.
+    Includes a list of exercises in the workout.
+    """
+
     workout = Workout.objects.get(pk=workout_id)
     workout_exercise = WorkoutExercise.objects.filter(workout=workout_id)
     return render(
@@ -47,6 +55,10 @@ def workout_detail(request, workout_id):
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def exercise_detail(request, workout_exercise_id):
+    """
+    Display details for a specific exercise.
+    """
+
     workout_exercise = WorkoutExercise.objects.get(pk=workout_exercise_id)
     return render(
         request, "workouts/exercise_detail.xml", {"workout_exercise": workout_exercise}
