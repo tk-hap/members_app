a5f3dd00841748c6d48a977a67ad3b69cd43ddc4
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-01-03 17:30:12 +1300

    update tenants

--- members_app/tenant_manager/apps.py
+++ members_app/tenant_manager/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TenantManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tenant_manager'
+    verbose_name = "Tenant Manager"
