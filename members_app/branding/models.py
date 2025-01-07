3da73ea161cf3919da05607599a01522a7a44765
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-01-07 16:48:52 +1300

    fix navbar

--- members_app/branding/models.py
+++ members_app/branding/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Branding(models.Model):
     logo = models.ImageField(upload_to="branding/logos/")
     primary_color = models.CharField(null=True, blank=True, default="#6750A4", max_length=255)
-    secondary_color= models.CharField(null=True, blank=True, max_length=255)
+    secondary_color= models.CharField(null=True, blank=True, default="#E8DEF8", max_length=255)
 
     def __str__(self):
         return "Global Branding Settings"
