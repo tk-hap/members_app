7c302cc4f24aa459d53f2d9a9a2e4f14b465b69b
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-01-30 09:30:39 +1300

    add whitenoise

--- members_app/members_app/settings.py
+++ members_app/members_app/settings.py
@@ -101,6 +101,7 @@ INSTALLED_APPS = SHARED_APPS + [
 MIDDLEWARE = [
     "django_tenants.middleware.main.TenantMainMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -132,6 +133,12 @@ TEMPLATES = [
         },
     },
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 WSGI_APPLICATION = "members_app.wsgi.application"
 
@@ -194,7 +201,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
